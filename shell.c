71e8286d2d7f58bd92e586c22100aecf24bd3bee
Jason Hatton <jason.a.hatton@gmail.com>
2015-11-30 18:47:05 -0600

    Implemented cd

--- shell.c
+++ shell.c
@@ -5,38 +5,29 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
-#define MAX_CMD_LENGTH 128
-#define MAX_PARAMS 16
-
-#define TRUE 1
-#define FALSE 0
-
+#define MAX_CMD_LENGTH 		128
+#define MAX_PARAMS 			16
 int run(char** param);
 void get(char * command, char** param);
-
 int main()
 {
-	
-	
 	char* param[1 + MAX_PARAMS];
 	char cmd[1 + MAX_CMD_LENGTH];
-	
-	while(TRUE)
+	while(1)
 	{
 		char* uname = getenv("USER");
+
 		printf("%s@topgnushell -->", uname);
-		
 		if(fgets(cmd, sizeof(cmd), stdin) == NULL) break;
 		cmd[strlen(cmd)-1] = (cmd[strlen(cmd)-1] == '\n') ? '\0' : cmd[strlen(cmd)-1];
-		
 		get(cmd, param);
 		if(strcmp(param[0], "exit") == 0) break;
+		if(strcmp(param[0], "cd") == 0)
+			if(chdir(param[1]) == -1) printf("Line 36");
 		if(run(param) == 0) break;
 	}
 	return 0;
 }
-
 int run(char** param)
 {
 	pid_t p = fork();
@@ -57,7 +48,6 @@ int run(char** param)
 		return 1;
 	}
 }
-
 void get(char* command, char** param)
 {
 	int i;
