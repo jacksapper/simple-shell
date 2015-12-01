f3d5bb7d3496d6b1ddce828389a2685d10f4f6ca
Jason Hatton <jason.a.hatton@gmail.com>
2015-11-30 18:21:18 -0600

    Initial shell commit.  It does commands but doesn't correctly do cd yet.  It can't change its working directory,

---
+++ shell.c
@@ -0,0 +1,69 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_CMD_LENGTH 128
+#define MAX_PARAMS 16
+
+#define TRUE 1
+#define FALSE 0
+
+int run(char** param);
+void get(char * command, char** param);
+
+int main()
+{
+	
+	
+	char* param[1 + MAX_PARAMS];
+	char cmd[1 + MAX_CMD_LENGTH];
+	
+	while(TRUE)
+	{
+		char* uname = getenv("USER");
+		printf("%s@topgnushell -->", uname);
+		
+		if(fgets(cmd, sizeof(cmd), stdin) == NULL) break;
+		cmd[strlen(cmd)-1] = (cmd[strlen(cmd)-1] == '\n') ? '\0' : cmd[strlen(cmd)-1];
+		
+		get(cmd, param);
+		if(strcmp(param[0], "exit") == 0) break;
+		if(run(param) == 0) break;
+	}
+	return 0;
+}
+
+int run(char** param)
+{
+	pid_t p = fork();
+	if(p < 0)
+	{
+		printf("cannot fork process");
+		return 1;
+	}
+	else if (p == 0)
+	{
+		execvp(param[0], param);
+		return 0;
+	}
+	else
+	{
+		int c;
+		waitpid(p, &c, 0);
+		return 1;
+	}
+}
+
+void get(char* command, char** param)
+{
+	int i;
+	for(i = 0; i < MAX_PARAMS; i++)
+	{
+		param[i] = strsep(&command, " ");
+		if(param[i] == NULL) break;
+	}
+}
